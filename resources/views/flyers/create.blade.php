93b72a8833fdd02da9b3147089563b93d3c22026
Zhidenko Nikolay <dragg.ko@gmail.com>
2015-08-09 22:12:15 +0300

    Add create form

--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -2,4 +2,8 @@
 
 @section('content')
     <h1>Selling Your Home?</h1>
+
+    <form method="post" action="{{ URL::route('flyers.store') }}" enctype="multipart/form-data">
+        @include('flyers.form')
+    </form>
 @stop
