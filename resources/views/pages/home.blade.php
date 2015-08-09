f7c0aba332e033d8dc8a3f62d8742f1bc85a314d
Zhidenko Nikolay <dragg.ko@gmail.com>
2015-08-09 11:18:51 +0300

    Make flyer resource controller

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="jumbotron">
+        <h1>Project Flyer</h1>
+
+        <p>
+            This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting
+            point to create something more unique by building on or modifying it.
+        </p>
+
+        <a href="{{ URL::route('flyers.create') }}" class="btn btn-primary">Create a Flyer</a>
+    </div>
+
+
+@stop
