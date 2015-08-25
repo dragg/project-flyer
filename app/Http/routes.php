a3dd6cbda7111b0bcc1c05adf235d41a3cc06d81
Zhidenko Nikolay <dragg.ko@gmail.com>
2015-08-25 22:51:31 +0300

    Do authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,8 +4,19 @@ Route::get('/', function () {
     return view('pages.home');
 });
 
-Route::resource('flyers', 'FlyersController');
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-Route::get('{zip}/{street}', 'FlyersController@show');
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
-Route::post('{zip}/{street}/photos', ['as' => 'store_photo_path', 'uses' => 'FlyersController@addPhoto']);
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('flyers', 'FlyersController');
+
+    Route::get('{zip}/{street}', 'FlyersController@show');
+
+    Route::post('{zip}/{street}/photos', ['as' => 'store_photo_path', 'uses' => 'FlyersController@addPhoto']);
+});
--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -3,19 +3,11 @@
 @section('content')
     <h1>Selling Your Home?</h1>
 
-    <div class="row">
-        <form method="post" action="{{ URL::route('flyers.store') }}" enctype="multipart/form-data" class="col-md-6">
-            @include('flyers.form')
+    <hr>
 
-            @if(count($errors) > 0)
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-        </form>
-    </div>
+    <form method="post" action="{{ URL::route('flyers.store') }}" enctype="multipart/form-data">
+        @include('flyers.form')
+
+        @include('errors')
+    </form>
 @stop
